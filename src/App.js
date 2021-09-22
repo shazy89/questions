eb8b0441e43c77042ae370cda30f6879ad236ceb
Ed <erdo.shazy123@gmail.com>
2021-09-21 21:34:46 -0400

    remove the unused code from app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import data from './api/local/questions.json';
+//import data from './api/local/questions.json';
 import { QuestionsDashboard } from './pages/questions-dashboard';
 import { setQuestions } from 'redux/actions/questions-fetch';
 import { Loading } from 'layout/loading';
 
 function App({ setQuestions, loading }) {
-  //  const [questionInfo, setQuestionInfo] = useState(null);
-
-  //  const displayQuestion = useCallback(() => {
-  //    setQuestionInfo(questions.find((question) => question.status === 'start'));
-  //  }, [questions]);
-
   useEffect(() => {
     setQuestions();
   }, [setQuestions]);
-  // useEffect(() => {
-  //   displayQuestion();
-  // }, [displayQuestion, questions]);
 
   return <>{loading ? <Loading /> : <QuestionsDashboard />}</>;
 }
@@ -25,7 +16,6 @@ function App({ setQuestions, loading }) {
 const mapStateProps = (state) => {
   return {
     loading: state.questions.loading,
-    // questions: state.questions.questions,
   };
 };
 export default connect(mapStateProps, { setQuestions })(App);
